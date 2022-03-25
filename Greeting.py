d4e09b4ab2ff6c00940a77b8012cae4e6a568f3a
TimmStr <timm.straub@gmx.de>
2022-03-25 20:36:53 +0100

    Target Function added

--- Greeting.py
+++ Greeting.py
@@ -18,13 +18,26 @@ class Greeting:
         counter=self.structureVariables
         for equa in range(0, self.equations):
             for struc in range(0, self.structureVariables):
-                x = int(input(('Type the '+str(struc+1)+'. strucure variable of the '+str(equa+1))+'. equation: '))
+                x = float(input(('Type the '+str(struc+1)+'. strucure variable of the '+str(equa+1))+'. equation: '))
                 self.arrays[equa][struc]=x
                 print(self.arrays)
             self.arrays[equa][counter]=1
             counter = counter + 1
-            x = int(input(('Type the b variable for the ' + str(equa + 1)) + '. equation: '))
+            x = float(input(('Type the b variable for the ' + str(equa + 1)) + '. equation: '))
             self.arrays[equa][self.structureVariables+self.equations]=x
             print(self.arrays)
+        self.addTargetFunction()
+
+
+    def addTargetFunction(self):
+        for struc in range(0,self.structureVariables):
+            x = float(input(('Type the ' + str(struc + 1) + '. structure variable of the target equation: ')))
+            self.arrays[self.equations][struc]=x
+        x = int(input(('If you want to maximize, type 1: ')))
+        if (x == 1):
+            for i in range(0,self.structureVariables):
+                self.arrays[self.equations][i]=self.arrays[self.equations][i]*-1
+        print(self.arrays)
+
 
 g = Greeting()
